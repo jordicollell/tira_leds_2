bf948878f4871c033f5189395bba192bb071bfb2
jordicollell <78138546+jordicollell@users.noreply.github.com>
2022-02-12 00:04:23 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -2,16 +2,19 @@ radio.onReceivedNumber(function (receivedNumber) {
     if (receivedNumber == 1) {
         strip.clear()
         cotxe1 = cotxe1 + 1
-        strip.setPixelColor(cotxe1, neopixel.colors(NeoPixelColors.Red))
-        strip.setPixelColor(cotxe2, neopixel.colors(NeoPixelColors.Red))
+        strip.setPixelColor(cotxe1, neopixel.colors(NeoPixelColors.Purple))
+        strip.setPixelColor(cotxe2, neopixel.colors(NeoPixelColors.Orange))
         strip.show()
     } else if (receivedNumber == 2) {
         strip.clear()
         cotxe2 = cotxe2 + 1
-        strip.setPixelColor(cotxe1, neopixel.colors(NeoPixelColors.Red))
-        strip.setPixelColor(cotxe2, neopixel.colors(NeoPixelColors.Red))
+        strip.setPixelColor(cotxe1, neopixel.colors(NeoPixelColors.Purple))
+        strip.setPixelColor(cotxe2, neopixel.colors(NeoPixelColors.Orange))
         strip.show()
     }
+})
+input.onButtonPressed(Button.AB, function () {
+    control.reset()
 })
 let cotxe2 = 0
 let cotxe1 = 0
@@ -19,8 +22,12 @@ let strip: neopixel.Strip = null
 radio.setGroup(1)
 strip = neopixel.create(DigitalPin.P0, 300, NeoPixelMode.RGB)
 strip.clear()
-cotxe1 = 0
-cotxe2 = 0
+strip.show()
+cotxe1 = 1
+cotxe2 = 1
+strip.setPixelColor(cotxe1, neopixel.colors(NeoPixelColors.Purple))
+strip.setPixelColor(cotxe2, neopixel.colors(NeoPixelColors.Orange))
+strip.show()
 basic.showLeds(`
     . . . . .
     . . . . .
